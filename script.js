414cdf07da4dee750e8c8967bcb53815576e6843
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-03-19 12:31:12 -0300

    Correção do problema com ID

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const idList = []
+
 function addTask() {
     let taskName = document.getElementById('input-new-task').value
 
@@ -5,22 +7,39 @@ function addTask() {
         taskName = 'Sem titulo'
     }
 
-    const idName = taskName.replace(/\s+/g, '')
+    let idName = taskName.replace(/\s+/g, '')
+   
+    if (idList.includes(idName)) {
+        let number = 1
+        let numberString = 0
+        let oldId = idName
+        while(idList.includes(idName)) {
+            idName = oldId
+            numberString = number.toString()
+            idName = idName + numberString
+            number = number + 1            
+        }
+
+        idList.push(idName)
+    } else {
+        idList.push(idName)
+    }
 
     const taskList = document.querySelector('.task-list')
     const newTask = document.createElement('div')
+    newTask.setAttribute('class', 'task')
+    newTask.setAttribute('id', idName)
 
     newTask.innerHTML = `
-        <div class="task">
-            <div>
-                <label>${taskName}</label>
-            </div>
-            <div class="task-icons">
-                <i class="fa-solid fa-circle-check" id="${idName}" onclick="completed(this.id)"></i>
-                <i class="fa-solid fa-circle-xmark" id="${idName}" onclick="deleteTask(this.id)"></i>
-                <i class="fa-solid fa-pen" id="${idName}" onclick="editTask(this.id)"></i>
-            </div>
-        </div>`
+        <div>
+            <label>${taskName}</label>
+        </div>
+        <div class="task-icons">
+            <i class="fa-solid fa-circle-check" id="${idName}" onclick="completed(this.id)"></i>
+            <i class="fa-solid fa-circle-xmark" id="${idName}" onclick="deleteTask(this.id)"></i>
+            <i class="fa-solid fa-pen" id="${idName}" onclick="editTask(this.id)"></i>
+        </div>
+        `
 
     taskList.insertBefore(newTask, taskList.querySelector('h2').nextSibling)
 
@@ -28,54 +47,51 @@ function addTask() {
 }
 
 function completed(id) {
-    const idName = document.getElementById(id)
-    const divLabel = idName.parentNode.previousElementSibling
+    const taskContainer = document.getElementById(id)
+    const divLabel = taskContainer.children[0].children[0]
     const taskName = divLabel.textContent.trim()
 
     const completedList = document.querySelector('.completed')
     const taskCompleted = document.createElement('div')
+    taskCompleted.setAttribute('class', 'task-completed')
+    taskCompleted.setAttribute('id', id)
 
     taskCompleted.innerHTML = `
-        <div class="task-completed">
             <div>
                 <label><del>${taskName}</del></label>
             </div>
             <div class="task-icons">
-                <i class="fa-solid fa-trash" id="${idName}" onclick="deleteTask(this.id)"></i>
-            </div>
-        </div>`
+                <i class="fa-solid fa-trash" id="${id}" onclick="deleteTask(this.id)"></i>
+            </div>`
 
     completedList.insertBefore(taskCompleted, completedList.querySelector('h2').nextSibling)
 
-    const oldTask = idName.parentNode.parentNode.parentNode
-
-    oldTask.remove()
+    taskContainer.remove()
 }
 
 function deleteTask(id) {
-    const idName = document.getElementById(id)
+    const taskContainer = document.getElementById(id)
 
-    const oldTask = idName.parentNode.parentNode.parentNode
-
-    oldTask.remove()
+    taskContainer.remove()
+    
+    idList.splice(idList.indexOf(id), 1)
 }
 
 function editTask(id) {
 
-    const idName = document.getElementById(id)
+    const taskContainer = document.getElementById(id)
+    const Label = taskContainer.children[0].children[0]
+    const labelValue = Label.textContent.trim()
 
-    const divLabel = idName.parentNode.previousElementSibling
-    const labelValue = divLabel.textContent.trim()
+    Label.remove()
+    taskContainer.children[0].innerHTML = `<input type="text" id="input-new-task" value="${labelValue}">`
 
-    divLabel.children[0].remove()
-    divLabel.innerHTML = `<input type="text" id="input-new-task" value="${labelValue}">`
-
-    const input = divLabel.children[0]
+    const input = taskContainer.children[0].children[0]
     input.focus()
     input.selectionStart = input.value.length
     input.selectionEnd = input.value.length
 
-    const divIcons = idName.parentNode
+    const divIcons = taskContainer.children[1]
     divIcons.classList.remove('task-icons')
     divIcons.classList.add('edit-task')
     while (divIcons.firstChild) {
@@ -87,30 +103,32 @@ function editTask(id) {
         <i class="fa-regular fa-circle-check" id="confirm"></i>
         <i class="fa-regular fa-circle-xmark" id="cancel"></i>
     `
+
+
     const confirm = document.getElementById('confirm')
     confirm.addEventListener('click', () => {
         const newValue = input.value
 
-        divLabel.children[0].remove()
-        divLabel.innerHTML = `<label>${newValue}</label>`
-        
+        taskContainer.children[0].children[0].remove()
+        taskContainer.children[0].innerHTML = `<label>${newValue}</label>`
+
         divIcons.classList.remove('edit-task')
         divIcons.classList.add('task-icons')
         while (divIcons.firstChild) {
             divIcons.removeChild(divIcons.firstChild)
         }
         divIcons.innerHTML = `
-            <i class="fa-solid fa-circle-check" id="${idName}" onclick="completed(this.id)"></i>
-            <i class="fa-solid fa-circle-xmark" id="${idName}" onclick="deleteTask(this.id)"></i>
-            <i class="fa-solid fa-pen" id="${idName}" onclick="editTask(this.id)"></i>
+            <i class="fa-solid fa-circle-check" id="${id}" onclick="completed(this.id)"></i>
+            <i class="fa-solid fa-circle-xmark" id="${id}" onclick="deleteTask(this.id)"></i>
+            <i class="fa-solid fa-pen" id="${id}" onclick="editTask(this.id)"></i>
         `
     })
 
     const cancel = document.getElementById('cancel')
     cancel.addEventListener('click', () => {
-        
-        divLabel.children[0].remove()
-        divLabel.innerHTML = `<label>${labelValue}</label>`
+
+        taskContainer.children[0].children[0].remove()
+        taskContainer.children[0].innerHTML = `<label>${labelValue}</label>`
 
         divIcons.classList.remove('edit-task')
         divIcons.classList.add('task-icons')
@@ -118,25 +136,9 @@ function editTask(id) {
             divIcons.removeChild(divIcons.firstChild)
         }
         divIcons.innerHTML = `
-            <i class="fa-solid fa-circle-check" id="${idName}" onclick="completed(this.id)"></i>
-            <i class="fa-solid fa-circle-xmark" id="${idName}" onclick="deleteTask(this.id)"></i>
-            <i class="fa-solid fa-pen" id="${idName}" onclick="editTask(this.id)"></i>
-        `
-    })
-
-    input.addEventListener('blur', () => {
-        divLabel.children[0].remove()
-        divLabel.innerHTML = `<label>${labelValue}</label>`
-
-        divIcons.classList.remove('edit-task')
-        divIcons.classList.add('task-icons')
-        while (divIcons.firstChild) {
-            divIcons.removeChild(divIcons.firstChild)
-        }
-        divIcons.innerHTML = `
-            <i class="fa-solid fa-circle-check" id="${idName}" onclick="completed(this.id)"></i>
-            <i class="fa-solid fa-circle-xmark" id="${idName}" onclick="deleteTask(this.id)"></i>
-            <i class="fa-solid fa-pen" id="${idName}" onclick="editTask(this.id)"></i>
+            <i class="fa-solid fa-circle-check" id="${id}" onclick="completed(this.id)"></i>
+            <i class="fa-solid fa-circle-xmark" id="${id}" onclick="deleteTask(this.id)"></i>
+            <i class="fa-solid fa-pen" id="${id}" onclick="editTask(this.id)"></i>
         `
     })
 
